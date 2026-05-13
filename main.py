d0eb735c6bacc03f3f4d25b453227f7082a7b87d
ddurumboy2197 <ddurumboy2197@gmail.com>
2026-05-13 16:21:06 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,10 @@
+def chiqish():
+    while True:
+        soz = input("Termagun: ")
+        if soz.lower() == "chiqish":
+            print("Xayr, foydalanuvchi!")
+            break
+        else:
+            print("Boshqa termagun!")
+
+chiqish()
